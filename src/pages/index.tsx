e664cbbbaffbe602e968b6357f7a71223d5e73df
Tim <harvard90873@gmail.com>
2021-08-27 15:58:52 +0200

    create plain button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>TAILWIND</div>;
+  return (
+    <main className="container min-h-screen bg-black">
+      <section className="flex justify-center items-center h-96">
+        {/* plain button with background */}
+        <button className="bg-cyan text-white w-28 h-10 rounded">
+          Click Me
+        </button>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
