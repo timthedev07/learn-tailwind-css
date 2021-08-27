2e3bf07b4a65594724d81609d311e853e15a0619
Tim <harvard90873@gmail.com>
2021-08-27 17:12:24 +0200

    import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
+import { Buttons } from "../components/Buttons";
 
 const Home: NextPage = () => {
-  return <main className="container min-h-screen bg-black"></main>;
+  return (
+    <main className="container min-h-screen bg-black">
+      <Buttons />
+    </main>
+  );
 };
 
 export default Home;
