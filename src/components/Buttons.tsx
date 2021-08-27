800cecdcc5ffc464fb6654e918c1dbf25c26295b
Tim <harvard90873@gmail.com>
2021-08-27 17:12:11 +0200

    split code up

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+export const Buttons: FC = () => {
+  const baseButtonClassName = "bg-cyan text-white w-28 h-10 rounded";
+  return (
+    <section className="flex justify-around items-center h-96 flex-col">
+      {/* plain button with background */}
+      <button className={baseButtonClassName}>I have nothing</button>
+
+      {/* button with hover state */}
+      <button className={`${baseButtonClassName} hover:bg-cyanHover`}>
+        Hover over me
+      </button>
+
+      {/* button with focus state */}
+      <button
+        className={`${baseButtonClassName} focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50`}
+      >
+        Click Me
+      </button>
+
+      {/* button with active state */}
+      <button className={`${baseButtonClassName} active:bg-cyanActive`}>
+        Click me and hold
+      </button>
+    </section>
+  );
+};
