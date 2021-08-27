8972ecded80e9ae142f13754597523ca110cd293
Tim <harvard90873@gmail.com>
2021-08-27 16:02:08 +0200

    create hover

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const baseButtonClassName = "bg-cyan text-white w-28 h-10 rounded";
   return (
     <main className="container min-h-screen bg-black">
-      <section className="flex justify-center items-center h-96">
+      <section className="flex justify-around items-center h-96 flex-col">
         {/* plain button with background */}
-        <button className="bg-cyan text-white w-28 h-10 rounded">
+        <button className={baseButtonClassName}>Click Me</button>
+
+        {/* button with hover state */}
+        <button className={`${baseButtonClassName} hover:bg-cyanHover`}>
           Click Me
         </button>
       </section>
