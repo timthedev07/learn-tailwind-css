046042d474c20435c0233ff4183949825af3a4d4
Tim <harvard90873@gmail.com>
2021-08-27 17:08:28 +0200

    complete active state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,23 @@ const Home: NextPage = () => {
     <main className="container min-h-screen bg-black">
       <section className="flex justify-around items-center h-96 flex-col">
         {/* plain button with background */}
-        <button className={baseButtonClassName}>Click Me</button>
+        <button className={baseButtonClassName}>I have nothing</button>
 
         {/* button with hover state */}
         <button className={`${baseButtonClassName} hover:bg-cyanHover`}>
+          Hover over me
+        </button>
+
+        {/* button with focus state */}
+        <button
+          className={`${baseButtonClassName} focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50`}
+        >
           Click Me
+        </button>
+
+        {/* button with active state */}
+        <button className={`${baseButtonClassName} active:bg-cyanActive`}>
+          Click me and hold
         </button>
       </section>
     </main>
