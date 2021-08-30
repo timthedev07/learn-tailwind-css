a8ffac0398f9004913135979735c95fb086fef1d
Tim <harvard90873@gmail.com>
2021-08-30 22:06:16 +0200

    create transitions.tsx

---
+++ src/components/Transitions.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+export const Transitions: FC = () => {
+  return (
+    <button className="btn transition hover:bg-cyanElementBackdrop transform hover:scale-110"></button>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from "next";
 import { Breakpoints } from "../components/Breakpoints";
 import { States } from "../components/States";
+import { Transitions } from "../components/Transitions";
 
 const Home: NextPage = () => {
   return (
     <main className="container min-h-screen bg-black min-w-full">
       <States />
       <Breakpoints />
+      <Transitions />
     </main>
   );
 };
