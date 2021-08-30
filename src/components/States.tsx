65cd5cdca1920f047ff01fc8c36c85799df664e2
Tim <harvard90873@gmail.com>
2021-08-30 20:06:26 +0200

    use custom utility class

--- src/components/States.tsx
+++ src/components/States.tsx
@@ -1,35 +1,26 @@
 import { FC } from "react";
 
 export const States: FC = () => {
-  const baseButtonClassName = "bg-cyan text-white w-auto px-4 h-10 rounded";
   return (
     <section className=" bg-cyanElementBackdrop flex justify-around items-center h-96 flex-col">
       {/* plain button with background */}
-      <button className={baseButtonClassName}>I have nothing</button>
+      <button className="btn">I have nothing</button>
 
       {/* button with hover state */}
-      <button className={`${baseButtonClassName} hover:bg-cyanHover`}>
-        Hover over me
-      </button>
+      <button className={`btn hover:bg-cyanHover`}>Hover over me</button>
 
       {/* button with focus state */}
       <button
-        className={`${baseButtonClassName} focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50`}
+        className={`btn focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50`}
       >
         Click Me
       </button>
 
       {/* button with active state */}
-      <button className={`${baseButtonClassName} active:bg-cyanActive`}>
-        Click and hold
-      </button>
+      <button className={`btn active:bg-cyanActive`}>Click and hold</button>
 
       {/* disabled button */}
-      <button
-        className={`${baseButtonClassName} active:bg-cyanActive opacity-50`}
-      >
-        Disabled
-      </button>
+      <button className={`btn opacity-50`}>Disabled</button>
 
       {/* checkbox */}
       <div className="p-4 max-w-xs mx-auto bg-white rounded-xl shadow-md">
