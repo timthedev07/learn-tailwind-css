e5281740ac10b4bf36a38f2efc888022ccc48ca0
Tim <harvard90873@gmail.com>
2021-08-30 23:00:20 +0200

    complete transitions.tsx

--- src/components/Transitions.tsx
+++ src/components/Transitions.tsx
@@ -2,6 +2,16 @@ import { FC } from "react";
 
 export const Transitions: FC = () => {
   return (
-    <button className="btn transition hover:bg-cyanElementBackdrop transform hover:scale-110"></button>
+    <section className="flex bg-gray-900 justify-around items-center h-96 flex-col">
+      <button className="btn transition duration-200 ease-in delay-75 hover:bg-cyanElementBackdrop transform hover:scale-110">
+        Hover
+      </button>
+      <button className="btn transition duration-500 ease-out delay-500 hover:bg-cyanElementBackdrop transform hover:scale-110">
+        Hover
+      </button>
+      <button className="btn transition duration-1000 ease-in-out delay-1000 hover:bg-cyanElementBackdrop transform hover:scale-110">
+        Hover
+      </button>
+    </section>
   );
 };
