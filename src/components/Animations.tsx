d1ee82405f0edb07faa8e6fbdc42ea6a952dd81b
Tim <harvard90873@gmail.com>
2021-08-31 12:55:05 +0200

    complete animations.tsx

--- src/components/Animations.tsx
+++ src/components/Animations.tsx
@@ -1,5 +1,21 @@
 import { FC } from "react";
 
 export const Animations: FC = () => {
-  return <section></section>;
+  return (
+    <section className="flex bg-black text-white justify-center items-center h-auto flex-col">
+      <img className="animate-spin w-96 h-96" src="/images/react.svg" alt="" />
+      <img className="animate-ping w-96 h-96" src="/images/react.svg" alt="" />
+      <img className="animate-pulse w-96 h-96" src="/images/react.svg" alt="" />
+      <img
+        className="animate-bounce w-96 h-96"
+        src="/images/react.svg"
+        alt=""
+      />
+      <img
+        className="animate-wiggle w-96 h-96"
+        src="/images/react.svg"
+        alt=""
+      />
+    </section>
+  );
 };
